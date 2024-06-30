6f4f6db08739c66bfe63c10b75c486686a0d29c0
FerCeli <arq.fceli@gmail.com>
2024-06-30 13:40:38 -0300

    Agregar base de datos

---
+++ Books_bd.sql
@@ -0,0 +1,41 @@
+create table Books_bd (
+	id INT,
+	title VARCHAR(50),
+	author VARCHAR(50),
+	genre VARCHAR(11),
+	publication_date DATE,
+	publisher VARCHAR(50),
+	price DECIMAL(5,2),
+	language VARCHAR(50),
+	cantidad INT
+);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (96, 'Rev', 'Pansie Frill', 'fiction', '11/2/2003', 'Edgeblab', 98.81, 'Persian', 26);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (39, 'Rev', 'Cam Binyon', 'romance', '5/14/1909', 'Dynava', 32.02, 'Japanese', 15);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (40, 'Dr', 'Floyd Temlett', 'fiction', '2/5/2007', 'Twitterlist', 1.88, 'Punjabi', 69);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (52, 'Mr', 'Martin Luffman', 'mystery', '9/8/1929', 'Zoomcast', 33.82, 'Dhivehi', 75);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (44, 'Dr', 'Rutledge Genike', 'romance', '4/30/1978', 'Quinu', 25.43, 'Mongolian', 19);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (94, 'Dr', 'Rubia Allibone', 'romance', '11/14/1920', 'Katz', 44.79, 'Malay', 14);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (67, 'Ms', 'Portia Keavy', 'mystery', '5/11/1983', 'Voonyx', 82.58, 'Irish Gaelic', 31);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (84, 'Rev', 'Hannie Richardon', 'mystery', '9/4/1961', 'Eadel', 31.82, 'Kashmiri', 31);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (62, 'Dr', 'Killy Rimour', 'romance', '4/28/1953', 'Topiclounge', 77.21, 'Japanese', 42);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (70, 'Dr', 'Elizabet Torrisi', 'non-fiction', '4/19/2005', 'Midel', 46.2, 'Azeri', 52);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (43, 'Mr', 'Edi Melendez', 'romance', '8/26/1906', 'Bluejam', 7.12, 'Albanian', 77);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (52, 'Rev', 'Maurene Chaters', 'mystery', '1/13/1965', 'Twiyo', 80.02, 'Tamil', 11);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (72, 'Ms', 'Isador Wensley', 'non-fiction', '3/11/1918', 'Topicstorm', 63.33, 'Japanese', 98);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (86, 'Mrs', 'Benedetto Duffy', 'romance', '1/16/1991', 'Zoozzy', 74.75, 'Chinese', 76);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (96, 'Mr', 'Nessi Wicklen', 'non-fiction', '9/2/1992', 'Dablist', 68.36, 'Guaraní', 94);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (45, 'Rev', 'Donnell Stoyles', 'mystery', '3/17/1966', 'Cogilith', 18.14, 'Malay', 41);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (42, 'Dr', 'Jere Buyers', 'non-fiction', '11/2/2008', 'Bubblemix', 15.71, 'Nepali', 67);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (91, 'Dr', 'Kelcie Whittle', 'romance', '5/16/1926', 'Browsetype', 65.47, 'Swati', 15);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (22, 'Dr', 'Samuel Curston', 'fiction', '1/23/2021', 'Jabbersphere', 68.82, 'Catalan', 88);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (92, 'Mr', 'Barron Aldous', 'non-fiction', '2/15/1946', 'Gabtune', 20.07, 'Gagauz', 18);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (66, 'Ms', 'Rosetta Lorking', 'mystery', '3/6/1957', 'Linklinks', 20.17, 'Tamil', 22);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (28, 'Mr', 'Kata Nunnerley', 'mystery', '11/10/1933', 'Dabvine', 93.37, 'Burmese', 65);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (86, 'Mr', 'Sergeant Gornal', 'romance', '3/26/1993', 'Voomm', 79.9, 'Norwegian', 95);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (93, 'Honorable', 'Giustino Murford', 'mystery', '5/25/1991', 'Avamba', 33.68, 'Afrikaans', 62);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (27, 'Honorable', 'Colleen Blaiklock', 'mystery', '1/19/1960', 'Meedoo', 80.44, 'Aymara', 93);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (16, 'Rev', 'Erskine Henze', 'mystery', '10/23/1974', 'Eamia', 29.21, 'Belarusian', 95);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (44, 'Dr', 'Nial Heinish', 'fiction', '6/26/1907', 'Edgeblab', 60.02, 'Latvian', 21);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (6, 'Dr', 'Gisella Handover', 'mystery', '10/10/1964', 'Realcube', 7.89, 'Telugu', 80);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (5, 'Ms', 'Gregory Queste', 'mystery', '6/17/1921', 'Skipstorm', 7.68, 'Dutch', 90);
+insert into Books_bd (id, title, author, genre, publication_date, publisher, price, language, cantidad) values (23, 'Ms', 'Nellie Averill', 'fiction', '1/21/1973', 'Devpoint', 12.83, 'Pashto', 97);
